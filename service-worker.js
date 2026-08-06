b7fb1ad9234e0ba7bc00cc048192fa5782002d8b
Iwona1974 <iwona.bj@gmail.com>
2026-08-06 20:44:14 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+const CACHE = 'atlas-v1-1';
+const CORE = ['./','index.html','css/style.css?v=19-final','js/script.js?v=19-final','manifest.webmanifest','icons/icon-192.png','icons/icon-512.png'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request)));
+});
