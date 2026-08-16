7b88cd661b3177b7b2fa812d29d8977f5b0f6f53
Iwona1974 <iwona.bj@gmail.com>
2026-08-16 21:46:08 +0200

    Remove service worker cache

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,10 @@
-const CACHE="atlas-fast-media-final-1";
-self.addEventListener("install",e=>e.waitUntil(self.skipWaiting()));
-self.addEventListener("activate",e=>e.waitUntil(
- caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())
-));
-self.addEventListener("fetch",e=>{
- if(e.request.method!=="GET")return;
- const u=new URL(e.request.url);
- if(u.pathname.includes("/assets/"))return;
- e.respondWith(fetch(e.request,{cache:"no-store"}));
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+  );
 });
